67f05e15a47eb078e31eb06d0a8d1dcae9ac9422
Hashinur Rashid Sohan <mhashinurrashidsohan199@gmail.com>
2025-07-23 13:13:16 +0600

    Initial commit

---
+++ src/com/mortgagecalculator/core/Main.java
@@ -0,0 +1,18 @@
+package com.mortgagecalculator.core;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Principal: ");
+
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
+
